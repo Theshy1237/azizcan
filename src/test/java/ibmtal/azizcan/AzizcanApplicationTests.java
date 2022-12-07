795b595a0c342ef86f3bf97a5f19709edc26ebca
azizcankabadayı <azizcankabadayi844@gmail.com>
2022-12-07 10:03:07 +0300

    Initial commit

---
+++ src/test/java/ibmtal/azizcan/AzizcanApplicationTests.java
@@ -0,0 +1,13 @@
+package ibmtal.azizcan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzizcanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
